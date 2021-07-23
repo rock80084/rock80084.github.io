71122314e008bf4355f80381e02c36b1af4bd4a5
rock80084 <rock800084@gmail.com>
2021-07-23 21:24:50 +0800

    add

--- 20210723-2.py
+++ 20210723-2.py
@@ -1,7 +1,19 @@
 
-    
+1.
+c=10
+if c%2==0:
+    print("偶數")
+else:
+    print("奇數")
+
+2.
 b=lambda x : "偶數" if (x%2==0) else "奇數"
-print(b(11))
+print(b(10))
+
+
 
 a=list(filter(lambda x:x%2==0,[1,2,3,4,5]))
 print(a)
+
+#unicod
+#r raw
